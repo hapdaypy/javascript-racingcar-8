4134dfb1b865c24a216fa903b6b59cf7dc32d385
unknown <alsdnrskfk85@naver.com>
2025-10-24 21:45:38 +0900

    feat: 문자열을 ,로 구분하여 새로운 배열에 저장

--- src/App.js
+++ src/App.js
@@ -2,10 +2,14 @@ import { Console } from '@woowacourse/mission-utils';
 
 class App {
   async run() {
-    const stringName = await Console.readLineAsync(
+    const carName = await Console.readLineAsync(
       '경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)',
     );
     const tryNumber = await Console.readLineAsync('시도할 횟수는 몇 회인가요?');
+    const carNameLength = carName.length;
+
+    const splitCarName = carName.split(',');
+    console.log(splitCarName);
   }
 }
 
