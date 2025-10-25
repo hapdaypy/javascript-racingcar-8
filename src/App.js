eede2ab0db926ab749e6cd58dee898c42266f4fb
unknown <alsdnrskfk85@naver.com>
2025-10-25 22:25:04 +0900

    feat: 자동차 이름 문자열 마지막에 ,가 있을 경우 프로그램 종료

--- src/App.js
+++ src/App.js
@@ -5,10 +5,16 @@ class App {
     const carName = await Console.readLineAsync(
       '경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)',
     );
-    const tryNumber = await Console.readLineAsync('시도할 횟수는 몇 회인가요?');
     const carNameLength = carName.length;
+    if (',' == carName[carNameLength - 1]) {
+      // 문자열 마지막에 , 가 있을 경우 ERROR를 출력하고 종료
+      Console.print('ERROR');
+      return;
+    }
 
-    const splitCarName = carName.split(',');
+    const tryNumber = await Console.readLineAsync('시도할 횟수는 몇 회인가요?');
+
+    const splitCarName = carName.split(','); // ,를 기준으로 자동차 이름 나누기
     console.log(splitCarName);
   }
 }
