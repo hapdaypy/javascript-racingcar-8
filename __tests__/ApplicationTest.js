4aae2f17b371439f0106eac121a559034fd521f8
unknown <alsdnrskfk85@naver.com>
2025-10-27 11:43:16 +0900

    test: 테스트 케이스 추가

--- __tests__/ApplicationTest.js
+++ __tests__/ApplicationTest.js
@@ -1,5 +1,5 @@
-import App from "../src/App.js";
-import { MissionUtils } from "@woowacourse/mission-utils";
+import App from '../src/App.js';
+import { MissionUtils } from '@woowacourse/mission-utils';
 
 const mockQuestions = (inputs) => {
   MissionUtils.Console.readLineAsync = jest.fn();
@@ -19,18 +19,18 @@ const mockRandoms = (numbers) => {
 };
 
 const getLogSpy = () => {
-  const logSpy = jest.spyOn(MissionUtils.Console, "print");
+  const logSpy = jest.spyOn(MissionUtils.Console, 'print');
   logSpy.mockClear();
   return logSpy;
 };
 
-describe("자동차 경주", () => {
-  test("기능 테스트", async () => {
+describe('자동차 경주', () => {
+  test('기능 테스트', async () => {
     // given
     const MOVING_FORWARD = 4;
     const STOP = 3;
-    const inputs = ["pobi,woni", "1"];
-    const logs = ["pobi : -", "woni : ", "최종 우승자 : pobi"];
+    const inputs = ['pobi,woni', '1'];
+    const logs = ['pobi : -', 'woni : ', '최종 우승자 : pobi'];
     const logSpy = getLogSpy();
 
     mockQuestions(inputs);
@@ -46,15 +46,29 @@ describe("자동차 경주", () => {
     });
   });
 
-  test("예외 테스트", async () => {
+  test('예외 테스트', async () => {
     // given
-    const inputs = ["pobi,javaji"];
+    const inputs = ['pobi,javaji'];
     mockQuestions(inputs);
 
     // when
     const app = new App();
 
     // then
-    await expect(app.run()).rejects.toThrow("[ERROR]");
+    await expect(app.run()).rejects.toThrow('[ERROR]');
   });
+});
+
+test.each([
+  { inputs: ['pobi,javaji'], description: '자동차 이름이 5자 초과' },
+  { inputs: ['pobi,woni', '-1'], description: '이동 횟수가 1보다 작음' },
+  { inputs: ['pobi,woni', '3.5'], description: '이동 횟수가 소수' },
+  { inputs: ['pobi,woni', 'abc'], description: '이동 횟수 숫자가 아님' },
+  { inputs: [''], description: '이름이 공백 입력' },
+])('예외 테스트 - $description', async ({ inputs }) => {
+  mockQuestions(inputs);
+
+  const app = new App();
+
+  await expect(app.run()).rejects.toThrow('[ERROR]');
 });
